c8e2ee94d4c2b0f65a283e72b0f4a48963f3f928
yukisato <yuki@yuki-sato.com>
2021-04-01 08:42:29 +0900

    remove onece

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from './App';
-export * from './Worker';
-export * from './authMiddleware';
-/**
- * User Required to be import Obniz for argument.
- * So it must be exported from this library
- */
-import Obniz from 'obniz';
-export { Obniz };
--- dist/install.d.ts
+++
@@ -1,2 +0,0 @@
-import { Installed_Device } from "obniz-cloud-sdk/sdk";
-export declare function getInstallRequest(token: string): Promise<Installed_Device[]>;

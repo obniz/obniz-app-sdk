37baf0522ba74d430be24f8aa82b4af1f4846368
kazuemon <git@k6n.jp>
2022-03-07 17:41:09 +0900

    docs: create redis-multi-master

---
+++ examples/clustered/redis-multi-master/manager1.js
@@ -0,0 +1,18 @@
+const { App, AppInstanceType, Worker } = require('../../../dist')
+const Obniz = require("obniz");
+
+/**
+ * AppInstanceType.Manager never start workers inside of itself. It just manage workers.
+ */
+const app = new App({
+  appToken: process.env.APPTOKEN,
+  instanceType: AppInstanceType.Manager,
+  instanceName: 'manager1',
+  database: "redis",
+  databaseConfig: process.env.REDIS_URL || "redis://localhost:6379",
+  obnizClass: Obniz
+})
+
+app.start({
+  port: 3333
+});

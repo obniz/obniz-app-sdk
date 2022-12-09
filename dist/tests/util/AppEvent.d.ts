d5578fd699a0c20fba0223996bd6cf28a7b05c87
KoheiKido <bouningenn@gmail.com>
2022-12-09 15:46:15 +0900

    remove dist

--- dist/tests/util/AppEvent.d.ts
+++
@@ -1,6 +0,0 @@
-import { AppEvent } from '../../ObnizCloudClient';
-export declare const appEvnetSamples: AppEvent[];
-export declare const appEventAddSamples: AppEvent[];
-export declare const appEventDeleteSamples: AppEvent[];
-export declare const appEventDeleteAllDeviceSamples: AppEvent[];
-export declare const appEventDeleteAndUpdateSamples: AppEvent[];
--- dist/worker_store/WorkerStoreBase.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WorkerStoreBase = void 0;
-class WorkerStoreBase {
-}
-exports.WorkerStoreBase = WorkerStoreBase;
-//# sourceMappingURL=WorkerStoreBase.js.map

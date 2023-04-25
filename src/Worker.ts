5b8cd4d3ff93ebb9c4b5cfb732085613ed36c45a
kazuemon <git@k6n.jp>
2023-04-25 14:49:21 +0900

    update: `Worker.install` の削除を差し戻して非推奨に変更

--- src/Worker.ts
+++ src/Worker.ts
@@ -11,6 +11,10 @@ import { DeviceInfo } from './types/device';
  * Example: https://qiita.com/okdyy75/items/610623943979cf422775#%E3%81%BE%E3%81%82%E3%81%A8%E3%82%8A%E3%81%82%E3%81%88%E3%81%9A%E3%81%A9%E3%82%93%E3%81%AA%E6%84%9F%E3%81%98%E3%81%AB%E6%9B%B8%E3%81%8F%E3%81%AE
  */
 export class Worker<O extends IObniz> {
+  /**
+   * @deprecated Please use deviceInfo instead.
+   */
+  public install: DeviceInfo;
   public deviceInfo: DeviceInfo;
   protected app: App<O>;
   protected obniz: O;
@@ -20,6 +24,7 @@ export class Worker<O extends IObniz> {
   private _cloudSdk: ReturnType<typeof getSdk> | null;
 
   constructor(deviceInfo: DeviceInfo, app: App<O>, option: IObnizOptions = {}) {
+    this.install = deviceInfo;
     this.deviceInfo = deviceInfo;
     this.app = app;
     this._obnizOption = option;
